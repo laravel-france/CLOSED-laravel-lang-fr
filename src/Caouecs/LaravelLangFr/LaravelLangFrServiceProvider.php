d82456cd55220607ffbe8abaf69d1ec924a0c21b
caouecs <caouecs@caouecs.net>
2013-05-29 17:17:43 +0200

    Initial

---
+++ src/Caouecs/LaravelLangFr/LaravelLangFrServiceProvider.php
@@ -0,0 +1,44 @@
+<?php namespace Caouecs\LaravelLangFr;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaravelLangFrServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Bootstrap the application events.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		$this->package('caouecs/laravel-lang-fr');
+	}
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array();
+	}
+
+}
---
+++ src/lang/fr/reminders.php
@@ -0,0 +1,22 @@
+<?php
+
+return array(
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reminder Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    "password" => "Les mots de passe doit avoir au moins six caractères et correspondre à la confirmation.",
+
+    "user"     => "Nous ne pouvons trouver cet utilisateur avec cette adresse e-mail.",
+
+    "token"    => "Ce jeton de réinitialisation de mot de passe n'est pas valide.",
+
+);
